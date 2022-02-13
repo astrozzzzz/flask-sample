ea4fe7af4eb769f45de6dfb311296239a9f8c85a
AstroZzz <nikitalaz59@mail.ru>
2022-02-13 18:16:18 +0500

    fourth commit

--- main.py
+++ main.py
@@ -82,5 +82,26 @@ def choice(planet):
 """
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Результаты</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="static/css/style.css"/>
+                  </head>
+                  <body>
+                    <h1 class="header">Результаты отбора</h1>
+                    <div class="alert alert-secondary">Претендента на участие в миссии {nickname}:</div>
+                    <div class="alert alert-success">Поздравляем! Ваш рейтинг после {level} этапа отбора составляет {rating}!</div>
+                    <div class="alert alert-secondary">Желаем удачи!</div>
+                  </body>
+                </html>
+"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
