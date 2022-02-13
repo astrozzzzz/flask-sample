1e8a73209b94aa0ba99d73a536a36ba75bf074bf
AstroZzz <nikitalaz59@mail.ru>
2022-02-13 18:08:29 +0500

    third commit

--- main.py
+++ main.py
@@ -59,5 +59,28 @@ def promotion_image():
                 </html>
 """
 
+
+@app.route('/choice/<planet>')
+def choice(planet):
+    return f"""
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Колонизация</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="static/css/style.css"/>
+                  </head>
+                  <body>
+                    <h1 class="header">Моё предложение: {planet}</h1>
+                    <div class="alert alert-secondary">Есть кислород</div>
+                    <div class="alert alert-success">Нет инопланетян</div>
+                    <div class="alert alert-secondary">Множество неизвестных ресурсов</div>
+                    <div class="alert alert-warning">Находится в соседней галактике</div>
+                  </body>
+                </html>
+"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
